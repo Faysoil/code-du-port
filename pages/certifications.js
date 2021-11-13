ba7b0f39d4d80e66cae00fa076cb5d60710dcc37
Faysoil <91457367+Faysoil@users.noreply.github.com>
2021-11-13 16:29:57 +0100

    fix: portfolio add files

---
+++ pages/certifications.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Certifications from "../components/Certifications";
+
+export default function certifications() {
+  return (
+    <ContainerBlock title="Certifications - Faysoil Bacari">
+      <Certifications />
+    </ContainerBlock>
+  );
+}
