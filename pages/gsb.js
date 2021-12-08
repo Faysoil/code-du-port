df04b8a4ed953455c05e069d5933e724d847f4a5
Faysoil <91457367+Faysoil@users.noreply.github.com>
2021-12-08 23:39:38 +0100

    fix: sending to school

---
+++ pages/gsb.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Contact from "../components/GSB";
+
+export default function gsb() {
+    return (
+        <ContainerBlock>
+            <Contact />
+        </ContainerBlock>
+    );
+}
